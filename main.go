74431781237267bc32514322c7721e72fcb8dec7
jaovw <joojvw@gmail.com>
2025-05-20 22:10:20 -0300

    go-api: - gorm - config for future database - schema model

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/jaovw/go-api/config"
 	"github.com/jaovw/go-api/router"
 )
 
 func main() {
+	err := config.Init()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	router.Initialize()
 }
